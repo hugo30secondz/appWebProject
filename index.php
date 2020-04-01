96fcf33c237196023fc9f0c25cf4f48e9574cc2b
hugo30secondz <hugo30secondz@hotmail.com>
2020-03-31 18:09:04 -0600

    cambio de comentarios

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include 'view/vistaComponentes.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
     <title>PROYECTO</title>
 </head>
@@ -59,7 +59,7 @@ include 'view/vistaComponentes.php';
     </nav>
 
     <?php 
-        if(isset($_SESSION['idUsuario'])){// si está logueado muestra para publicar
+        if(isset($_SESSION['idUsuario'])){// si está logueado muestra pantallas para cada usuario dependiendo el tipo
             if($_SESSION['tipo'] == "registrado"){
             ?>
                 <h1> USUARIO REGISTRADO </h1>
