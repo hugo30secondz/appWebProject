a617099d7edeb010e66d44c69a6ceebcdf6949e0
hugo30secondz <hugo30secondz@hotmail.com>
2020-04-06 23:54:40 -0500

    2do avance likes

---
+++ controller/controlerLikes.php
@@ -0,0 +1,39 @@
+<script>
+function controlLike(v,n) {
+
+  if(v === "bien"){
+    if(document.getElementById('bien'+n).className === "btn btn-success"){
+      document.getElementById('bien'+n).className = "btn btn-light";
+    }
+    else{
+      document.getElementById('bien'+n).className = "btn btn-success";
+      document.getElementById('regular'+n).className = "btn btn-light";
+      document.getElementById('mal'+n).className = "btn btn-light";
+    }
+  }
+  else{
+    if(v === "regular"){
+      if(document.getElementById('regular'+n).className === "btn btn-warning"){
+        document.getElementById('regular'+n).className = "btn btn-light";
+      }
+      else{
+        document.getElementById('regular'+n).className = "btn btn-warning";
+        document.getElementById('bien'+n).className = "btn btn-light";
+        document.getElementById('mal'+n).className = "btn btn-light";
+      }
+    }
+    else{
+      if(v === "mal"){
+        if(document.getElementById('mal'+n).className === "btn btn-danger"){
+          document.getElementById('mal'+n).className = "btn btn-light";
+        }
+        else{
+          document.getElementById('mal'+n).className = "btn btn-danger";
+          document.getElementById('bien'+n).className = "btn btn-light";
+        document.getElementById('regular'+n).className = "btn btn-light";
+        }
+      }
+    }
+  }
+}
+</script>
--- view/vistaVerPosts.php
+++ view/vistaVerPosts.php
@@ -9,54 +9,14 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-  <script>
-  function controlLike(v,n) {
-
-    if(v === "bien"){
-      if(document.getElementById('bien'+n).className === "btn btn-success"){
-        document.getElementById('bien'+n).className = "btn btn-light";
-      }
-      else{
-        document.getElementById('bien'+n).className = "btn btn-success";
-        document.getElementById('regular'+n).className = "btn btn-light";
-        document.getElementById('mal'+n).className = "btn btn-light";
-      }
-    }
-    else{
-      if(v === "regular"){
-        if(document.getElementById('regular'+n).className === "btn btn-warning"){
-          document.getElementById('regular'+n).className = "btn btn-light";
-        }
-        else{
-          document.getElementById('regular'+n).className = "btn btn-warning";
-          document.getElementById('bien'+n).className = "btn btn-light";
-          document.getElementById('mal'+n).className = "btn btn-light";
-        }
-      }
-      else{
-        if(v === "mal"){
-          if(document.getElementById('mal'+n).className === "btn btn-danger"){
-            document.getElementById('mal'+n).className = "btn btn-light";
-          }
-          else{
-            document.getElementById('mal'+n).className = "btn btn-danger";
-            document.getElementById('bien'+n).className = "btn btn-light";
-          document.getElementById('regular'+n).className = "btn btn-light";
-          }
-        }
-      }
-    }
-    //document.getElementById('bien').className = "btn btn-success";
-    //console.log('Gracias por pinchar');
-  }
-</script>
 </head>
 <body>
  
 <div class="container">
   <h2> Bienvenido</h2>
   <?php 
-include '../controller/conexion.php'; 
+include '../controller/conexion.php';
+include '../controller/controlerLikes.php'; 
 foreach ($mbd->query("SELECT *, publicaciones.id, categoria.tipo, registrado.usuario_id, usuario.nombre FROM (((publicaciones INNER JOIN categoria ON publicaciones.categoria_id = categoria.id) INNER JOIN registrado ON registrado.id = publicaciones.registrado_id) INNER JOIN usuario ON usuario.id = registrado.usuario_id ) WHERE aprobado =0 ") as $row){ // aca puedes hacer la consulta e iterarla con each. ?> 
 <div class="card text-center">
   <div class="card-header" style="background-color:#FDDDCA">
