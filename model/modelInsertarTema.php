f27ed43638aa019a7a0f2ba25a52be741afa58f3
PastorCisneros <pasada.65@gmail.com>
2020-04-12 23:19:52 -0500

    Validador de temas repetidos

--- model/modelInsertarTema.php
+++ model/modelInsertarTema.php
@@ -13,12 +13,35 @@ echo "</br>";
 echo $foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
 echo "</br>";
 */
-if(isset($_POST['tema'])){   
+
+
+    if(isset($_POST['tema'])){   
 
         try {
             $gsent = $mbd->prepare("SELECT id FROM registrado WHERE usuario_id = '".$_SESSION['idUsuario']."' ");
             $gsent->execute();
             $id = $gsent->fetch(PDO::FETCH_ASSOC);
+
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "pro_appweb";
+            
+            // Creamos la conexión
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            $tema=$_POST['tema'];
+            $sql = "SELECT * FROM categoria WHERE tipo='$tema'";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result)>0)
+            {
+                echo "<script> alert('Tu tema esta repetido! ');  window.location.assign('../index.php'); </script>";
+                //header('Location: ../index.php');
+            }
+           else{
+                // Realizamos la consulta para saber si coincide con uno de esos criterios
+            $sql = "select tema from categoria where tipo='$tema'";
+            $result = mysqli_query($conn, $sql);
 
             //insertamos en la tabla usuario
             $sql = "INSERT INTO categoria (tipo) VALUES ('".$_POST["tema"]."' )" ;
@@ -29,6 +52,10 @@ if(isset($_POST['tema'])){
 
             echo "<script> alert('tu tema ha sido creado:) ');  window.location.assign('../index.php'); </script>";
             //header('Location: index.php');
+
+           }
+               
+           
             }
         catch(PDOException $e)
             {
@@ -39,5 +66,8 @@ else
 {
     header('Location: ../index.php');
 }
+
+
+
 
 ?>
