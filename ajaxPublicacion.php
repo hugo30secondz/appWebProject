9877f0e569ba0fd1ef2485071131c0214be98914
hugo30secondz <hugo30secondz@hotmail.com>
2020-04-16 13:15:18 -0500

    practica ajax 2 archivos

---
+++ ajaxPublicacion.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<script>
+function showPost(id) {//funcion con ajax para mostrar las publicaciones mediante id
+    if (id == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","getPost.php?id="+id,true);
+        xmlhttp.send();
+    }
+}
+</script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script-->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+</head>
+<body>
+    <div id="txtHint">
+        <script>
+            showPost(4);
+        </script>
+    </div>
+
+    <br>
+
+</body>
+</html>
---
+++ getPost.php
@@ -0,0 +1,40 @@
+<?php
+    include 'controller/conexion.php';
+
+    $id = $_GET['id'];
+    $gsent = $mbd->prepare("SELECT * FROM publicaciones WHERE id = ".$id." ");;
+    $gsent->execute();
+    $row = $gsent->fetch(PDO::FETCH_ASSOC);
+
+    $modulo = '
+    <div class="card-body" style="background-color:#82CACD">
+        <h5 class="card-title">'.$row['id'].'</h5>
+        <p class="card-text">'.$row['texto'].'</p>
+        <img class="img-fluid" style="width:600px; height:350px;" id="fotoProducto" src="data:image/jpeg;base64,'.base64_encode($row['img']).'"/>
+        <br>
+        <div class="row">                    
+            <div class="col"></div>
+                <div class="row mt-2">
+                    <div class="col">
+                        <div class="alert alert-success" id="b'.$row['id'].'" role="alert">
+                            <a class="alert-link" id="lb'.$row['id'].'">'.$row['bien'].'</a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="alert alert-warning" id="r'.$row['id'].'" role="alert">
+                            <a class="alert-link" id="lr'.$row['id'].'">'.$row['regular'].'</a>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="alert alert-danger" id="m'.$row['id'].'" role="alert">
+                            <a class="alert-link" id="lm'.$row['id'].'">'.$row['mal'].'</a>
+                        </div>
+                    </div>
+                </div>
+            <div class="col"></div>
+        </div> 
+    </div>
+    ';
+    echo $modulo;
+?>
+
